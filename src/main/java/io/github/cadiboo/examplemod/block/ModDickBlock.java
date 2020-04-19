1c24cec0771168e41d7963d80091a657c6bb8e41
Jmar0025 <jamr0025@gmail.com>
2020-04-19 13:08:44 +0200

    first commit

---
+++ src/main/java/io/github/cadiboo/examplemod/block/ModDickBlock.java
@@ -0,0 +1,19 @@
+package io.github.cadiboo.examplemod.block;
+
+import io.github.cadiboo.examplemod.init.ModTileEntityTypes;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.IBlockReader;
+
+import javax.annotation.Nullable;
+
+public class ModDickBlock extends Block {
+
+    public ModDickBlock(Properties properties) {
+        super(properties);
+    }
+
+
+
+}
---
+++ src/main/java/io/github/cadiboo/examplemod/client/gui/ModDickScreen.java
@@ -0,0 +1,11 @@
+package io.github.cadiboo.examplemod.client.gui;
+
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.util.text.ITextComponent;
+
+public class ModDickScreen extends Screen {
+
+    protected ModDickScreen(ITextComponent titleIn) {
+        super(titleIn);
+    }
+}
